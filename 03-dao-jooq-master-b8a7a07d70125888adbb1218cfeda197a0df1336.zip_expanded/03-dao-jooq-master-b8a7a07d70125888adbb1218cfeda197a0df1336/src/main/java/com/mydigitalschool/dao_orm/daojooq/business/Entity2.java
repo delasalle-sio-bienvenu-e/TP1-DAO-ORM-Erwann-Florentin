e97feb743b53daba1e88e8dc557ece5a279f05f9
Bienvenu Erwann <erwannb.pro@gmail.com>
2017-12-08 15:39:41 +0100

    test push

--- 03-dao-jooq-master-b8a7a07d70125888adbb1218cfeda197a0df1336.zip_expanded/03-dao-jooq-master-b8a7a07d70125888adbb1218cfeda197a0df1336/src/main/java/com/mydigitalschool/dao_orm/daojooq/business/Entity2.java
+++
@@ -1,9 +0,0 @@
-package com.mydigitalschool.dao_orm.daojooq.business;
-
-public class Entity2 {
-    public Long id;
-
-    //... les deux propriétés min
-    // public ? property1;
-    // public ? property2;
-}
