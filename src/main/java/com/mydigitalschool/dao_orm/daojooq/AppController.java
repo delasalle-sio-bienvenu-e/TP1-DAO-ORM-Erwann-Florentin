ecb589411b650b298606e6ac46acd98b97cb2537
Florentin <Florentin@DESKTOP-230RCHK>
2017-12-11 16:16:30 +0100

    ManegeDao

--- src/main/java/com/mydigitalschool/dao_orm/daojooq/AppController.java
+++ src/main/java/com/mydigitalschool/dao_orm/daojooq/AppController.java
@@ -1,7 +1,7 @@
 package com.mydigitalschool.dao_orm.daojooq;
 
 import com.mydigitalschool.dao_orm.daojooq.business.manege;
-import com.mydigitalschool.dao_orm.daojooq.dao.Entity1Dao;
+import com.mydigitalschool.dao_orm.daojooq.dao.ManegeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,17 +12,17 @@ import java.util.List;
 // on aurait utilisé @Controller pour un contrôleur d'application web
 public class AppController {
     @Autowired
-    Entity1Dao entity1Dao;
+    ManegeDao entity1Dao;
 
 //    @Autowired
 //    Entity2Dao entity2Dao;
 
     public manege saveEntity1(manege entity1) throws SQLException {
-        return entity1Dao.saveEntity1(entity1);
+        return entity1Dao.saveManege(entity1);
     }
 
     public manege getEntity1(Long id) throws SQLException {
-        return entity1Dao.getEntity1(id);
+        return entity1Dao.getManege(id);
     }
 
     public <T> List<manege> getEntities1(T property1, int nbValuesToSkip, int nbValuesToReturn) {
--- src/main/java/com/mydigitalschool/dao_orm/daojooq/dao/ManegeDao.java
+++ src/main/java/com/mydigitalschool/dao_orm/daojooq/dao/ManegeDao.java
@@ -2,6 +2,8 @@ package com.mydigitalschool.dao_orm.daojooq.dao;
 
 import com.mydigitalschool.dao_orm.daojooq.business.manege;
 import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,20 +13,22 @@ import javax.sql.DataSource;
 import java.sql.SQLException;
 
 @Component
-public class Entity1Dao {
+public class ManegeDao {
     @Autowired
     DataSource dataSource;
 
-    public manege saveEntity1(manege entity1) throws SQLException {
-        // TODO à implémenter
+    public manege saveManege(manege manege) throws SQLException {
         DSLContext dbDSL = DSL.using(dataSource.getConnection(), SQLDialect.MYSQL);
         // voir https://www.jooq.org/doc/3.3/manual/sql-building/sql-statements/insert-statement/
+        dbDSL.insertInto(manege, manege.nom, manege.type, manege.nombrePlaces).values("grande roue", "familliale", 100);
         // dbDSL.insertInto(Entites1, ...).values(...)...
         throw new UnsupportedOperationException();
     }
 
-    public manege getEntity1(Long id) {
-        // TODO à implémenter
+    public manege getManege(Long id) throws SQLException {
+    	DSLContext dbDSL = DSL.using(dataSource.getConnection(), SQLDialect.MYSQL);
+    	manege manege = new manege();
+    	Result<Record> result = dbDSL.select().from(manege).where(manege.id = id).fetch();
         throw new UnsupportedOperationException();
     }
 }
