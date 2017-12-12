df8010fe5e9ae4d467c869bc063f441fcb1117d8
Florentin <Florentin@DESKTOP-230RCHK>
2017-12-12 16:45:27 +0100

    oula prank ca c'est mal passé

---
+++ 03-dao-jooq-97a0df1336/src/main/java/com/mydigitalschool/dao_orm/daojooq/business/Entity1.java
@@ -0,0 +1,11 @@
+package com.mydigitalschool.dao_orm.daojooq.business;
+
+public class Entity1 {
+    public Long id;
+    //... les deux propriétés min
+    // public ? property1;
+    // public ? property2;
+
+    // les composants multiples
+    //    public List<...> components;
+}
---
+++ 03-dao-jooq-97a0df1336/src/main/java/com/mydigitalschool/dao_orm/daojooq/business/Entity2.java
@@ -0,0 +1,9 @@
+package com.mydigitalschool.dao_orm.daojooq.business;
+
+public class Entity2 {
+    public Long id;
+
+    //... les deux propriétés min
+    // public ? property1;
+    // public ? property2;
+}
